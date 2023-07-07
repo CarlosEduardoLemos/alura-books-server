2e0dedc08b8d19b27f50ed664bfb626898b667b3
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-07-07 14:07:35 -0300

    Atualização do arquivo livros.js em controladores e exclusão de teste.js
    Excluí o arquivo "teste.js" que não era mais necessário.
    Atualizei o arquivo "livros.js" na pasta "controladores".
    Criei a pasta "controladores" e adicionei o arquivo "livros.js" nela.
    O arquivo "livros.js" em "controladores" agora contém uma função chamada "getLivros".
    A função "getLivros" é responsável por lidar com a solicitação de obtenção de livros e retorna uma resposta simples.
    O código do arquivo "livros.js" na pasta "serviços" permaneceu inalterado.

--- controladores/livros.js
+++ controladores/livros.js
@@ -1,10 +1,13 @@
-function getLivros(req, res){
-    try {
-        res.send('Olá mundo da Alura do Brasil!')
+const {getTodosLivros} = require("../servicos/livro")
+
+function getLivros(req, res) {
+    try{
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error){
         res.status(500)
         res.send(error.message)
-    }
+    } 
 }
 
 module.exports = {
